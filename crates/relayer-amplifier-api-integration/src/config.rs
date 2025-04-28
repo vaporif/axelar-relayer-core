d109ce21a924f63318de3fa57516868972bc9d89
Ivan Cavlek <ivan.cavlek1403@gmail.com>
2025-04-28 14:42:14 +0200

    feat: added default values

--- crates/relayer-amplifier-api-integration/src/config.rs
+++ crates/relayer-amplifier-api-integration/src/config.rs
@@ -23,14 +23,21 @@ pub struct Config {
         default = "config_defaults::get_chains_poll_interval",
         deserialize_with = "common_serde_utils::duration_ms_decode"
     )]
-    #[arg(env = "AMPLIFIER_API_CHAINS_POLL_INTERVAL", value_parser = parse_chains_poll_interval)]
+    #[arg(
+        env = "AMPLIFIER_API_CHAINS_POLL_INTERVAL", 
+        value_parser = parse_chains_poll_interval, 
+        default_value = config_defaults::chains_poll_interval_default_value().to_string()
+    )]
     pub get_chains_poll_interval: core::time::Duration,
 
     /// The max amount of tasks that we want to receive in a batch.
     /// This goes hand-in-hand with the `get_chains_poll_interval`
     #[builder(default = config_defaults::get_chains_limit())]
     #[serde(default = "config_defaults::get_chains_limit")]
-    #[arg(env = "AMPLIFIER_API_CHAINS_LIMIT")]
+    #[arg(
+        env = "AMPLIFIER_API_CHAINS_LIMIT", 
+        default_value = config_defaults::get_chains_limit().to_string()
+    )]
     pub get_chains_limit: u8,
 
     /// How often we check the liveliness of the Amplifier API
@@ -40,15 +47,22 @@ pub struct Config {
         default = "config_defaults::healthcheck_interval",
         deserialize_with = "common_serde_utils::duration_ms_decode"
     )]
-    #[arg(env = "AMPLIFIER_API_HEALTHCHECK_INTERVAL", value_parser = parse_healthcheck_interval)]
+    #[arg(
+        env = "AMPLIFIER_API_HEALTHCHECK_INTERVAL", 
+        value_parser = parse_healthcheck_interval, 
+        default_value = config_defaults::healthcheck_interval_default_value().to_string()
+    )]
     pub healthcheck_interval: core::time::Duration,
 
     /// How many invalid healthchecks do we need to do before we deem that the service is down and
     /// we should shut down the component
     #[builder(default = config_defaults::invalid_healthchecks_before_shutdown())]
     #[serde(default = "config_defaults::invalid_healthchecks_before_shutdown")]
-    #[arg(env = "AMPLIFIER_API_INVALID_HEALTHCHECKS_BEFORE_SHUTDOWN")]
-    pub invalid_healthchecks_before_shutdown: Option<usize>,
+    #[arg(
+        env = "AMPLIFIER_API_INVALID_HEALTHCHECKS_BEFORE_SHUTDOWN", 
+        default_value = config_defaults::invalid_healthchecks_before_shutdown().to_string()
+    )]
+    pub invalid_healthchecks_before_shutdown: usize,
 }
 
 fn parse_identity(input: &str) -> Result<Identity, String> {
@@ -75,16 +89,24 @@ pub(crate) mod config_defaults {
     use core::time::Duration;
 
     pub(crate) const fn healthcheck_interval() -> Duration {
-        Duration::from_secs(10)
+        Duration::from_secs(healthcheck_interval_default_value())
+    }
+
+    pub(crate) const fn healthcheck_interval_default_value() -> u64 {
+        10
     }
 
     pub(crate) const fn get_chains_poll_interval() -> Duration {
-        Duration::from_secs(10)
+        Duration::from_secs(chains_poll_interval_default_value())
+    }
+
+    pub(crate) const fn chains_poll_interval_default_value() -> u64 {
+        10
     }
 
     #[expect(clippy::unnecessary_wraps, reason = "fine for config defaults")]
-    pub(crate) const fn invalid_healthchecks_before_shutdown() -> Option<usize> {
-        Some(5)
+    pub(crate) const fn invalid_healthchecks_before_shutdown() -> usize {
+        5
     }
     pub(crate) const fn get_chains_limit() -> u8 {
         4
