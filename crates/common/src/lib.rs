57bef1bf0b3e9c758969902e9a922b339f337abe
Dmytro Onypko <vaporif@gmail.com>
2025-04-29 16:52:55 +0100

    cleanup

---
+++ crates/common/src/lib.rs
@@ -0,0 +1,30 @@
+//! Crate with amplifier component connectors.
+
+use tokio_util::sync::CancellationToken;
+
+pub fn register_backtrace() {
+    #[cfg(debug_assertions)]
+    {
+        // Note: unsafe only in multithreaded
+        unsafe {
+            std::env::set_var("RUST_BACKTRACE", "full");
+        }
+    }
+}
+
+/// Run from main thread once
+#[must_use]
+pub fn register_ctrlc_handler() -> CancellationToken {
+    let cancel_token = CancellationToken::new();
+    let ctrlc_token = cancel_token.clone();
+    ctrlc::set_handler(move || {
+        if ctrlc_token.is_cancelled() {
+            std::process::exit(1);
+        } else {
+            println!("\nGraceful shutdown initiated. Press Ctrl+C again for immediate exit...");
+            ctrlc_token.cancel();
+        }
+    })
+    .expect("Failed to register ctrl+c handler");
+    cancel_token
+}
